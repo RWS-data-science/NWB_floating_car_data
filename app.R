cad06d0e1c88befa69c094c044b9df8a49f2c06c
ajknol <ajknol@gmail.com>
2017-06-07 15:18:30 +0200

    c

--- app.R
+++ app.R
@@ -1,2 +1,13 @@
-#NWB shapefile inlezen
+#init
+rm(list=ls(all=TRUE))
+source("packages.R")
+source("functions.R")
+
+print(paste0("First copy NWB_WEGGEG_COMPLEET dir from P:/civ/RWS_DST/Data_droog to ", getwd(), "/db"))
+
+#NWB shapefile inlezen (31-12-2016)
+# wegvakken <- readOGR(dsn = paste0(getwd(),"/db/NWB_WEGGEG_COMPLEET/nwb/BN0112-a-Shape-R-U/Wegvakken/Wegvakken.shp"), layer = "Wegvakken")
+# wegvakken <- as.data.frame(wegvakken)
+# saveRDS(wegvakken, "db/NWB_wegvakken_raw.rds")
+wegvakken <- readRDS("db/NWB_wegvakken_raw.rds")
 
