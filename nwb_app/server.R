ed59f0ffcffc0d12aa20c7480b9c8a63a116fa40
martijnkoole <martijnkoole@gmail.com>
2017-06-23 16:54:05 +0200

    commit

--- nwb_app/server.R
+++ nwb_app/server.R
@@ -17,19 +17,30 @@ server<- function(input, output) {
 opa_nwb<- reactive({input$opa_nwb})
 opa_float<- reactive({input$opa_float})
 
-output$map = renderLeaflet(    leaflet() %>% addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
+output$map = renderLeaflet(    leaflet() %>% #addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
                                  #clearShapes() %>%
                                  setView(lng = 4.65, lat = 52.025, zoom = 12) 
 )
 
 observe({
+  if (input$kaart==T){
   leafletProxy("map") %>%
+    addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
     clearShapes() %>%
     addPolylines(data=strava,col="red",opacity = opa_float()) %>%
-    addPolylines(data=nwb_select,weight = 5, opacity = opa_nwb())
+    addPolylines(data=nwb_select,weight = 5, opacity = opa_nwb(),popup= ~paste("WVK_ID: ",WVK_ID))
+    
+  }
+  else {
+    leafletProxy("map") %>%
+      clearShapes() %>%
+      addPolylines(data=strava,col="red",opacity = opa_float()) %>%
+      addPolylines(data=nwb_select,weight = 5, opacity = opa_nwb(),popup= ~paste("WVK_ID: ",WVK_ID))
+  }
     
 
 })
-
 }
+
+
    
