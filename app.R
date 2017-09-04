2af015b5bf862fa5b3520000d845d0d9834dfdee
daanvandermaas <daanvandermaas@hotmail.com>
2017-09-04 14:59:22 +0200

    x

--- app.R
+++ app.R
@@ -1,5 +1,9 @@
 #generic function to turn a shape into a shape file with equally distance points
-
+source('prepare_shape.r')
 #function to make a neigrest neighbour table from two point files
 
 #A function to match lines in two shape files with help of the neighrest neighbour table of the points that where added to the lines in step 1
+
+
+
+
