95609f377635a78c6128011ec2a87f6b7ea1d863
daanvandermaas <daanvandermaas@hotmail.com>
2017-09-15 12:28:40 +0200

    x

---
+++ lib.r
@@ -0,0 +1,21 @@
+library(rPython)
+library(rgdal)
+library(sp)
+#library(tmap)
+library(sf)
+library(stats)
+library(pbapply)
+#library(gdalUtils)
+library(rlist)
+library(fields)
+library(data.table)
+library(ggplot2)
+<<<<<<< HEAD
+library(plyr)
+=======
+library(leaflet)
+>>>>>>> 2f1777a40df22e18d6c09fec7428386e1edb7be5
+
+
+
+
