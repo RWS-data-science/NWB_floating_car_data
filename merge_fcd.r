73894968d35725f9c53d68e9dc661f292b94ede3
daanvandermaas <daanvandermaas@hotmail.com>
2017-09-18 12:23:30 +0200

    x

--- merge_fcd.r
+++ merge_fcd.r
@@ -1,51 +1,54 @@
+
 dir = 'db/fcd'
-tijd1 = as.POSIXct( '2016-01-01 06:00:00', "%Y-%m-%d %H:%M:%S")
-tijd2 = as.POSIXct( '2016-01-01 19:00:00', "%Y-%m-%d %H:%M:%S")
+uur1 = 19
+uur2 = 6
 
-tabel = merge_tables(dir = dir, tijd1 = tijd1, tijd2 = tijd2)
-
-
+tabel = merge_tables(dir = dir,uur1 = uur1, uur2 = uur2)
 
 
 
-merge_tables = function(dir, tijd1, tijd2){
 
 
+merge_tables = function(dir, uur1, uur2){
+
+#maak vector van de uren die we meenemen
+  if(uur1<uur2){
+uren = uur1:uur2
+  }else{
+    uren = c(uur1:23, 0:uur2)
+  }
+  
+
+#zoek alle files in de directory
 files = list.files(dir)
 
 #initialiseer starttabel
-tabel = data.frame('SegmentID' = -1) 
+tabel = data.frame('SegmentID' = -1,'Coverage_totaal' = 0  ,'SpeedKph_totaal' = 0) 
 
 
 
 
 for(i in 1:length(files)){
 
-  #lees nieuwe tabel in
-
-  tijd = strsplit(readLines(paste(dir , files[i], sep = '/'), n = 1), '[:;]' )
-  tijd = paste0('2016-01-01 ',tijd[[1]][[2]], ':', tijd[[1]][[3]], ':00')
-  tijd = as.POSIXct(tijd, "%Y-%m-%d %H:%M:%S")
+  #lees de tijd van de nieuwe tabel
+  tijd = as.numeric(strsplit(readLines(paste(dir , files[i], sep = '/'), n = 1), '[:;]' )[[1]][[2]])
+ 
   
-  if(tijd > tijd1 & tijd < tijd2){ #als de tijd tussen de ingegeven grenzen valt voeg dan de informatie toe
+  if(tijd %in% uren){ #als de tijd tussen de ingegeven grenzen valt voeg dan de informatie toe
   
 tabel_nieuw = read.table( paste(dir , files[i], sep = '/') , sep = ';', skip = 1, header = TRUE)
-colnames(tabel_nieuw)[2:5] = paste(colnames(tabel_nieuw)[2:5], 'nieuw', sep = '_'  )
 
 #merge nieuwe tabel met de oude
 tabel =  merge(x = tabel, y = tabel_nieuw, key = 'SegmentID', all.x = TRUE, all.y = TRUE)
 
+
 #stel coverage en speed bij
 tabel[is.na(tabel)] = 0
 tabel_nieuw[is.na(tabel_nieuw)] = 0
-tabel$SpeedKph = tabel$SpeedKph+ tabel$SpeedKph_nieuw* tabel$Coverage_nieuw
-tabel$Coverage = tabel$Coverage  + tabel$Coverage_nieuw 
+tabel$SpeedKph_totaal = tabel$SpeedKph_totaal+ tabel$SpeedKph* tabel$Coverage
+tabel$Coverage_totaal = tabel$Coverage_totaal  + tabel$Coverage
 
-#verwijder de nieuwe kolomen weer
-tabel$Coverage_nieuw = NULL
-tabel$SpeedKph_nieuw = NULL
-tabel$LOS.Reference_nieuw = NULL
-tabel$TraveltimeMS_nieuw = NULL
+
 
 }#einde if
 
@@ -53,6 +56,16 @@ tabel$TraveltimeMS_nieuw = NULL
 
 #neem gemiddelde van snelheid
 tabel$SpeedKph = tabel$SpeedKph / tabel$Coverage
+
+#gooid segmentID van -1 weg
+tabel = tabel[ - which(tabel$SegmentID==1),] 
+
+
+#verwijder kolomen 
+tabel$Coverage = NULL
+tabel$SpeedKph = NULL
+tabel$LOS.Reference = NULL
+tabel$TraveltimeMS = NULL
 
 return(tabel)
 }
