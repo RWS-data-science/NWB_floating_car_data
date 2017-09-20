c79e448a070369148c6197a59a95560e5016c879
koolem <koolem@hadoop-1.ad.rws.nl>
2017-09-20 17:10:03 +0200

    c

---
+++ aggregeer_fcd.R
@@ -0,0 +1,18 @@
+library(foreign)
+base_full=read.dbf("db/basemaps/13354-shapes/segments.dbf")
+
+source('merge_fcd.r')
+
+dagen<- c("01","02","03","04","05","06","07","08","09" ,"10")
+
+
+for (dag in dagen){
+dir = paste0('db/06/',dag)
+uur1 = 6
+uur2 = 19
+
+tabel_overdag = merge_tables(dir = dir,uur1 = uur1, uur2 = uur2)
+save(tabel_overdag,file=paste0('db/fcd_agg/',dag,'_dag.RData'))
+tabel_nacht =  merge_tables(dir = dir,uur1 = uur2, uur2 = uur1)
+save(tabel_nacht,file=paste0('db/fcd_agg/',dag,'_nacht.RData'))
+}
