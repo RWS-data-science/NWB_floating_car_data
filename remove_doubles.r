5fa30ed12256584c6b8f035d9f924be761b2811a
daanvandermaas <daanvandermaas@hotmail.com>
2017-09-21 10:15:01 +0200

    x

--- remove_doubles.r
+++ remove_doubles.r
@@ -1,67 +1,117 @@
 
-#loop door alle lijnen
 
-#als twee opeenvolgende punten hetzelfde zijn als twee opeenvolgende punten in een line die al is opgenomen moet de line daar gesplit worden
-
-#alle lines bestaande uit 1 punt moeten geschrapt worden
-
-NWB = nwb_select
-NWB@lines = NWB@lines[1:10]
-#initialiseer de lijst van opgenomen lijnen
-NWB_no_doubles = list(NWB@lines[[1]]@Lines[[1]]@coords)
+OSM = basemap_select
+OSM@lines = OSM@lines[1:100]
 
 
-#loop door de lines van het NWB en bekijk iedere lijn als kandidaat
-for( i in 2:length(NWB@lines)){
+OSM_no_doubles = list(OSM@lines[[1]]@Lines[[1]]@coords)
+n=2
+for(i in 2:length(OSM@lines)){
   
-  print(i/ length(NWB@lines))
-  
-  split = data.frame('split' = rep(0, nrow(NWB@lines[[i]]@Lines[[1]]@coords)  ) )
-  n=0
+  x = OSM@lines[[i]]@Lines[[1]]@coords[ nrow(OSM@lines[[i]]@Lines[[1]]@coords):1 ,]
   
   
-  #loop door de punten van de kandidaat lijn
-  for(j in 2:nrow(NWB@lines[[i]]@Lines[[1]]@coords)){
-    
-    #loop door alle opgenomen lijnen
-    for( k in 1:length(NWB_no_doubles)){
-      
-      #verschil van de punten op de k-de opgenomen lijn met het j-de en j-1-de punt van de kandidaat lijn 
-      
-    
-      
-      
-      if( any(   abs(  rowSums( as.data.frame(NWB_no_doubles[[k]][-1,] - NWB@lines[[i]]@Lines[[1]]@coords[j,] ) )  ) < 1e-1 &   abs(  rowSums(  as.data.frame( (NWB_no_doubles[[k]][-nrow(NWB_no_doubles[[k]]),] - NWB@lines[[i]]@Lines[[1]]@coords[j-1,]) ) ) ) < 1e-1 )){
-        #als afstand tussen twee opeenvolgende punten 0 is dan 
-        n = n+1
-        
-      }
-      
-    
-      }#einde opgenomen lijnen loop
-    
-    
-    split$split[j] = n
-    
-  }#einde punten van kandidaat loop
   
-  #split het dataframe op alle punten dat
- extra =  split( as.data.frame(NWB@lines[[i]]@Lines[[1]]@coords), split$split )
-
- #verwijder lijnen die uit maar 1 coordinaat bestaan
- extra = lapply(c(1:length(extra)), function(l){
-   if(nrow(extra[[l]])>1){
-     return(extra[[l]])
-   }
- })
- extra = Filter(Negate(is.null), extra)
- 
-#voeg deze toe aan NWB_no_doubles  
-NWB_no_doubles = c(NWB_no_doubles, extra )
+  dubbel = lapply(1:length(OSM_no_doubles), function(j){
+    
+    if(nrow(OSM_no_doubles[[j]])== nrow(x)){
+   return( sum(OSM_no_doubles[[j]] == x) == ncol(x)*nrow(x))
+    }else{
+      return(0)
+    }
+  })
   
+  
+  
+  if(sum( unlist(dubbel))== 0){
+  OSM_no_doubles[[n]] =  OSM@lines[[i]]@Lines[[1]]@coords
+  n = n+1  
+  }
+   
+    
+    
+  
+  
+}
 
 
-}#einde kandidaten loop
 
 
 
+
+
+
+
+
+
+
+# 
+# #loop door alle lijnen
+# 
+# #als twee opeenvolgende punten hetzelfde zijn als twee opeenvolgende punten in een line die al is opgenomen moet de line daar gesplit worden
+# 
+# #alle lines bestaande uit 1 punt moeten geschrapt worden
+# 
+# NWB = basemap_select
+# NWB@lines = NWB@lines[1:100]
+# #initialiseer de lijst van opgenomen lijnen
+# NWB_no_doubles = list(NWB@lines[[1]]@Lines[[1]]@coords)
+# 
+# 
+# #loop door de lines van het NWB en bekijk iedere lijn als kandidaat
+# for( i in 2:length(NWB@lines)){
+#   
+#   print(i/ length(NWB@lines))
+#   
+#   NWB@lines[[i]]@Lines[[1]]@coords = NWB@lines[[i]]@Lines[[1]]@coords[nrow(NWB@lines[[i]]@Lines[[1]]@coords):1,]
+#   
+#   split = data.frame('split' = rep(0, nrow(NWB@lines[[i]]@Lines[[1]]@coords)  ) )
+#   n=0
+#   
+#   
+#   
+#   #loop door de punten van de kandidaat lijn
+#   for(j in 2:nrow(NWB@lines[[i]]@Lines[[1]]@coords)){
+#     
+#     #loop door alle opgenomen lijnen
+#     for( k in 1:length(NWB_no_doubles)){
+#       
+#       #verschil van de punten op de k-de opgenomen lijn met het j-de en j-1-de punt van de kandidaat lijn 
+#       
+#     
+#       
+#       
+#       if( any(   abs(  rowSums( as.data.frame(NWB_no_doubles[[k]][-1,] - NWB@lines[[i]]@Lines[[1]]@coords[j,] ) )  ) < 1e-1 &   abs(  rowSums(  as.data.frame( (NWB_no_doubles[[k]][-nrow(NWB_no_doubles[[k]]),] - NWB@lines[[i]]@Lines[[1]]@coords[j-1,]) ) ) ) < 1e-1 )){
+#         #als afstand tussen twee opeenvolgende punten 0 is dan 
+#         n = n+1
+#         
+#       }
+#       
+#     
+#       }#einde opgenomen lijnen loop
+#     
+#     
+#     split$split[j] = n
+#     
+#   }#einde punten van kandidaat loop
+#   
+#   #split het dataframe op alle punten dat
+#  extra =  split( as.data.frame(NWB@lines[[i]]@Lines[[1]]@coords), split$split )
+# 
+#  #verwijder lijnen die uit maar 1 coordinaat bestaan
+#  extra = lapply(c(1:length(extra)), function(l){
+#    if(nrow(extra[[l]])>1){
+#      return(extra[[l]])
+#    }
+#  })
+#  extra = Filter(Negate(is.null), extra)
+#  
+# #voeg deze toe aan NWB_no_doubles  
+# NWB_no_doubles = c(NWB_no_doubles, extra )
+#   
+# 
+# 
+# }#einde kandidaten loop
+# 
+# 
+# any(   abs(  rowSums( as.data.frame(x[[k]][-1,] - totaal[[i]][j,] ) )  ) < 1e-1 &   abs(  rowSums(  as.data.frame( (x[[4]][-nrow(x[[k]]),] - totaal[[i]][j-1,]) ) ) ) < 1e-1 )
--- test.r
+++ test.r
@@ -1,4 +1,4 @@
-shape = basemap_select@lines[1:10]
+shape = basemap_select@lines[1:100]
 
 
 shape = nwb_select@lines[1:100]
@@ -14,14 +14,26 @@ totaal$coor = paste0(totaal$V1, '_', totaal$V2)
 
 View(table(totaal$coor))
 
-106694.481077028   448203.227437655
+107809.601726209_446036.925749525
 
 
 totaal = lapply(1:length(shape), function(i){
   
- if( any( abs( shape[[i]]@Lines[[1]]@coords[,1] - 106694.481077028) < 1e-2  ) ){
+ if( any( abs( shape[[i]]@Lines[[1]]@coords[,1] - 107809.601726209) < 1e-2  ) ){
    return( as.data.frame(shape[[i]]@Lines[[1]]@coords))
  }
+})
+
+totaal = Filter(Negate(is.null), totaal)
+
+
+
+
+totaal = lapply(1:length(OSM_no_doubles), function(i){
+  
+  if( any( abs( OSM_no_doubles[[i]][,1] - 107809.601726209) < 1e-2  ) ){
+    return( as.data.frame( OSM_no_doubles[[i]] ))
+  }
 })
 
 totaal = Filter(Negate(is.null), totaal)
