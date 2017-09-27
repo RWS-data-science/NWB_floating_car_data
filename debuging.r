a1ccf46351c7c6f104c6015c7b4a9e926718eeea
daanvandermaas <daanvandermaas@hotmail.com>
2017-09-27 20:41:10 +0200

    x

--- debuging.r
+++ debuging.r
@@ -16,3 +16,6 @@ saveRDS(basemap_select, 'db/basemap_select.rds')
 
 nwb_select = readRDS("db/nwb_select.rds")
 basemap_select = readRDS('db/basemap_select.rds')
+
+saveRDS(juncties, 'db/juncties.rds')
+juncties = readRDS('db/juncties.rds')
