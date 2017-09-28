147c6e840e97f92a807e577116787c1e34600704
koolem <koolem@hadoop-1.ad.rws.nl>
2017-09-28 16:06:22 +0200

    c

--- aggregeer_fcd.R
+++ aggregeer_fcd.R
@@ -1,23 +1,45 @@
-library(foreign)
 base_full=read.dbf("db/basemaps/13354-shapes/segments.dbf")
 
 source('merge_fcd.r')
 
-dagen<- c(#"01","02","03","04","05","06",
-  "07","08","09" ,"10")
+
+dagen<- c("01","02","03","04","05","06",
+          "07","08","09" ,"10")
+
+dirs = paste0('db/06/',dagen)
+uur1 = 9
+uur2 = 19
+
+library(parallel)
+# Calculate the number of cores
+no_cores <- 6
+# Initiate cluster
+cl <- makeCluster(no_cores)
+
+list_overdag<- parLapply(cl, dirs,
+                         merge_tables,uur1 = uur1, uur2 = uur2)
 
 
-for (dag in dagen){
-  dir = paste0('db/06/',dag)
-  uur1 = 9
-  uur2 = 19
-
-  tabel_overdag <- merge_tables(dir = dir,uur1 = uur1, uur2 = uur2)
-  save(tabel_overdag,file=paste0('db/fcd_agg/',dag,'_dag.RData'))
-  tabel_nacht =  merge_tables(dir = dir,uur1 = uur2, uur2 = uur1)
-  save(tabel_nacht,file=paste0('db/fcd_agg/',dag,'_nacht.RData'))
-}
-
-
-
-
+# library(foreign)
+# base_full=read.dbf("db/basemaps/13354-shapes/segments.dbf")
+# 
+# source('merge_fcd.r')
+# 
+# dagen<- c(#"01","02","03","04","05","06",
+#   "07","08","09" ,"10")
+# 
+# 
+# for (dag in dagen){
+#   dir = paste0('db/06/',dag)
+#   uur1 = 9
+#   uur2 = 19
+# 
+#   tabel_overdag <- merge_tables(dir = dir,uur1 = uur1, uur2 = uur2)
+#   save(tabel_overdag,file=paste0('db/fcd_agg/',dag,'_dag.RData'))
+#   tabel_nacht =  merge_tables(dir = dir,uur1 = uur2, uur2 = uur1)
+#   save(tabel_nacht,file=paste0('db/fcd_agg/',dag,'_nacht.RData'))
+# }
+# 
+# 
+# 
+# 
--- merge_fcd.r
+++ merge_fcd.r
@@ -3,22 +3,24 @@
 
 
 merge_tables = function(dir, uur1, uur2){
+  library(foreign)
+  library(data.table)
+  #maak vector van de uren die we meenemen
+    if(uur1<uur2){
+  uren = uur1:uur2
+    }else{
+      uren = c(uur1:23, 0:uur2)
+    }
 
-#maak vector van de uren die we meenemen
-  if(uur1<uur2){
-uren = uur1:uur2
-  }else{
-    uren = c(uur1:23, 0:uur2)
-  }
   
-
+base_full=read.dbf("db/basemaps/13354-shapes/segments.dbf")
 #zoek alle files in de directory
 files = list.files(dir)
 
 #initialiseer starttabel
 #tabel = data.frame('SegmentID' = -1,'Coverage_totaal' = 0  ,'SpeedKph_totaal' = 0, 'aantal_keer_totaal' = 0) 
 
-tabel= data.frame('SegmentID'=base_full$segmentID,'Coverage_totaal' = 0  ,'SpeedKph_totaal' = 0, 'aantal_keer_totaal' = 0)
+tabel= data.frame('SegmentID'=base_full$segmentID,'Coverage_totaal' = 0  ,'SpeedKph_totaal' = 0, 'aantal_keer_totaal' = 0,'minuten_gelezen' =0)
 
 
 
@@ -26,8 +28,9 @@ for(i in 1:length(files)){
 #a<-Sys.time()
   #lees de tijd van de nieuwe tabel
   tijd = as.numeric(strsplit(readLines(paste(dir , files[i], sep = '/'), n = 1), '[:;T]' )[[1]][[2]])
+  basemap<- as.numeric(strsplit(readLines(paste(dir , files[i], sep = '/'), n = 1), ';' )[[1]][[2]])
  
-  
+  if(basemap == 13354){
   if(tijd %in% uren){ #als de tijd tussen de ingegeven grenzen valt voeg dan de informatie toe
   
     if (substr(files[i], nchar(files[i])-1, nchar(files[i])) == 'gz'){ #alleen .gz files
@@ -54,19 +57,18 @@ tabel$SpeedKph = NULL
 tabel$LOS.Reference = NULL
 tabel$TraveltimeMS = NULL
 tabel$aantal_keer = NULL
+
+tabel$minuten_gelezen <- tabel$minuten_gelezen+1
     }
   }#einde if
   if (i %% 100==0  ){print(i/length(files))}
 #print(Sys.time()-a)
+  }
+  
 }
 
 #neem gemiddelde van snelheid
 tabel$SpeedKph = tabel$SpeedKph_totaal / tabel$aantal_keer_totaal
-
-#gooid segmentID van -1 weg
-#tabel = tabel[ - which(tabel$SegmentID==-1),] 
-
-
 
 
 return(tabel)
